b50c7d51c2fe6f51fabd93ec2590e1e4b0f2c0ec
joejay1129 <zrl1644@mail.ustc.edu.cn>
2023-12-10 11:10:29 +0800

    fourth commit

--- inst/doc/intro.R
+++ inst/doc/intro.R
@@ -1,8 +1,37 @@
 ## -----------------------------------------------------------------------------
-add <- function(a,b){
-    return(a+b)
+IPW <- function(M, K) {
+    n <- nrow(M)
+    d <- ncol(M)
+    Y <- ifelse(M == 0, 0, 1) 
+    
+    p_hat <- sum(Y) / (n * d)
+    
+    Sigma_phat <- crossprod(M) - (1 - p_hat) * diag(colSums(M^2))
+    
+    eigen_Sigma_phat <- eigen(Sigma_phat)
+    
+    V <- eigen_Sigma_phat$vectors[, 1:K]
+    
+    return(V = V)
 }
 
+## -----------------------------------------------------------------------------
+distribute <- function(matrices_get) {
+    n <- nrow(matrices_get[[1]])
+    d <- ncol(matrices_get[[1]])
+    m <- length(matrices_get)
+    
+    Sigma_tilde <- matrix(0, d, d)
+    V <- vector("list", m)
+    
+    for (i in 1:m) {
+        V[[i]] <- IPW(matrices_get[[i]])
+        Sigma_tilde <- Sigma_tilde + (1 / m) * V[[i]] %*% t(V[[i]])
+    }
+    
+    V_res <- eigen(Sigma_tilde)$vec[, 1:2]
+    return(V_res)
+}
 
 ## -----------------------------------------------------------------------------
 library(Rcpp)
